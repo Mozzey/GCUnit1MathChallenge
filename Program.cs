d7368ffbe4a55b163f9f87d33c1ff729175e9316
Mozzey <mbcacciano@gmail.com>
2018-06-09 20:10:10 -0400

    validation revision

--- Program.cs
+++ Program.cs
@@ -16,24 +16,19 @@ namespace MathChallenge1
             {
                 //Greet the user and ask for input
                 Console.WriteLine("Hello there! Please enter two whole numbers of the same length.");
-                Console.WriteLine("This program will check the sums of all corresponding");
-
+                Console.WriteLine("This program will check the sums of all corresponding indexes in the two numbers");
                 //Store the user input
                 Console.Write("First Number : ");
                 string input1 = Console.ReadLine();
                 Console.Write("Second Number : ");
                 string input2 = Console.ReadLine();
-
                 // Run IndexSumsCompare method 
                 Console.WriteLine(IndexSumsCompare(input1, input2));
                 // Run again or terminate program based on user input
                 Console.WriteLine("Would you like to run the program again? Y / N");
                 string playAgain = Console.ReadLine();
-                if (playAgain == "Y" || playAgain == "y")
-                {
-                    isRunning = true;
-                }
-                else
+
+                if (!(playAgain == "Y" || playAgain == "y"))
                 {
                     isRunning = false;
                 }
@@ -42,51 +37,44 @@ namespace MathChallenge1
 
         private static bool IndexSumsCompare(string num1, string num2)
         {
-            // Validate that one or both inputs are not empty
-            if (num1 == "" || num2 == "")
+            // Check for null input 
+            if (num1.Length == 0 || num2.Length == 0)
             {
-                Console.WriteLine("Sorry but a there must be a value in both inputs");
+                Console.WriteLine("Sorry but both input fields need to be provided");
+                return false;
             }
             // Validate that the Length of both inputs match
             if (num1.Length != num2.Length)
             {
                 Console.WriteLine("Sorry but your numbers do not match in length...please try again");
+                return false;
             }
             // Validate both inputs are numbers
             if (!((num1.All(Char.IsDigit) && num2.All(Char.IsDigit))))
             {
                 Console.WriteLine("Sorry but both inputs must be whole numbers");
+                return false;
             }
-
-
             // Variable to store the sum of both numbers at index 1
             int sumToCompare = 0;
             for (int i = 0; i < num1.Length; i++)
             {
-                // try catch to handle index out of bounds Exception
-                try
+                // Variable that sets comparison on first run
+                // then is checked against on 2nd run
+                int indexSum = (int)num1[i] + (int)num2[i];
+                if (i == 0)
                 {
-                    // Variable that sets comparison on first run
-                    // then is checked against on 2nd run
-                    int indexSum = (int)num1[i] + (int)num2[i];
-                    if (i == 0)
-                    {
-                        // Set comparison on first run
-                        sumToCompare = indexSum;
-                    }
-                    else
-                    {
-                        // Compare with other index sums
-                        // If sums dont match on 2nd run program stops here
-                        if (sumToCompare != indexSum)
-                        {
-                            return false;
-                        }
-                    }
+                    // Set comparison on first run
+                    sumToCompare = indexSum;
                 }
-                catch (Exception)
+                else
                 {
-                    continue;
+                    // Compare with other index sums
+                    // If sums dont match on x run program stops here
+                    if (sumToCompare != indexSum)
+                    {
+                        return false;
+                    }
                 }
             }
             // All index sums match
